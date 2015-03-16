ea5d706ffcdba63e118cffd20d0ef34f02e4b489
Josh Crowther <jshcrowthe@gmail.com>
2015-03-16 15:00:05 -0600

    Fixing build process, slimming down dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,24 @@
 var gulp = require('gulp');
+var mainBowerFiles = require('main-bower-files');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
-gulp.task('build', function() {
+gulp.task('build_components', function() {
+  return gulp.src(mainBowerFiles(), { base: 'path/to/bower_components' })
+        .pipe(concat('components.js'))
+        .pipe(gulp.dest('./public/js'));
+});
+
+gulp.task('build_app', function() {
   return gulp.src(['./public/js/app/*.js', './public/js/app/**/*.js'])
+          .pipe(concat('angularApp.js'))
+          .pipe(gulp.dest('./public/js'));
+});
+
+gulp.task('build', ['build_components', 'build_app'], function() {
+  return gulp.src(['./public/js/components.js', './public/js/angularApp.js'])
           .pipe(concat('app.js'))
+          .pipe(uglify())
           .pipe(gulp.dest('./public/js'));
 });
 
